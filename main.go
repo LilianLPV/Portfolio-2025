b237b549823951cf6a74e4ea3dbe3796e3d0357c
LilianLPV <lilian.lepiver@ynov.com>
2026-04-01 15:33:56 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 func main() {
 	templates.InitTemplates()
 	http.HandleFunc("/", pages.PortfolioHandler)
-	http.HandleFunc("/", pages.AccueilHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	fmt.Println("Le serveur est lancé http://localhost:8081")
 	http.ListenAndServe(":8081", nil)
